aada8aa85953a12c2f5a77ef30a50e3cec844000
hzy-051201 <3765529270@qq.com>
2026-04-06 22:23:25 +0800

    Add files via upload

---
+++ lib/data/environment/env.g.dart
@@ -0,0 +1,22 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+final class _Env {
+  static const String dsApiKey = 'sk-34c089a3a7c14219a6282ff8e004780a';
+
+  static const String supaUrl = 'https://unjwofyrzunxxcfeejin.supabase.co';
+
+  static const String supaAnon =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVuandvZnlyenVueHhjZmVlamluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4MjI4MzYsImV4cCI6MjA4OTM5ODgzNn0.41HCAe-RziEg42h8ksxzVbFH9XN3arCSmiRTCvsj3XA';
+
+  static const String amapKey = '7fa1e4112323c82f3f588d14d84970a4';
+
+  static const String amapWeatherKey = '113a26741127c6d9e471612ffa5e1135';
+}
